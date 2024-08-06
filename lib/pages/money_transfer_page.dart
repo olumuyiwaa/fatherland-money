ec36da68d36f9ecc098a11f9b0074f3446958fbd
holumuyiwa-emnac <oladoyinemmanuel@gmail.com>
2024-08-06 21:24:35 +0100

    Initial commit

--- lib/pages/money_transfer_page.dart
+++ lib/pages/money_transfer_page.dart
@@ -14,7 +14,68 @@ class MoneyTransferPage extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
           child: ListView(
-            children: [],
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                height: 168,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/balance-1.png'),
+                  fit: BoxFit.fill,
+                )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Total Balance',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            Icon(
+                              Icons.remove_red_eye,
+                              color: Colors.white,
+                              size: 18,
+                            )
+                          ],
+                        ),
+                        Text(
+                          '#25,000.40',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            '#20,000',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          Text(
+                            '|',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          Text(
+                            '#20,000',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ));
   }
