e8ee04520ea45efefb5b985ae524a9f55a40d239
Emmanuel Oluwamuyiwa Oladoyin <oladoyinemmanuel@gmail.com>
2024-09-10 09:27:44 +0100

    "Rearranged import statements in TransactionLog.dart"

--- lib/pages/transaction_log.dart
+++ lib/pages/transaction_log.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
 
-import '../pages/profile.dart';
-
-import '../utilities/transation_history_card.dart';
 import 'package:flutter/material.dart';
-
 import '../utilities/Analytics_widget.dart';
+import '../pages/profile.dart';
+import '../utilities/transation_history_card.dart';
 
 class TransactionLog extends StatelessWidget {
   TransactionLog({super.key});
